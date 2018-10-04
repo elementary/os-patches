63a4676189ffab176400bf9d7990e787d5e1250c
Fernando da Silva Sousa <wild.nando@gmail.com>
2018-10-04 20:17:12 -0300

    remove macro to fix annotation warning

--- src/cloudproviders.h
+++ src/cloudproviders.h
@@ -1,8 +1,6 @@
 #ifndef CLOUDPROVIDERS_H
 #define CLOUDPROVIDERS_H
 
-G_BEGIN_DECLS
-
 #define CLOUDPROVIDERS_INSIDE
 #include "enums.h"
 #include "cloudprovidersaccount.h"
@@ -11,7 +9,5 @@ G_BEGIN_DECLS
 #include "cloudprovidersprovider.h"
 #include "cloudprovidersproviderexporter.h"
 #undef CLOUDPROVIDERS_INSIDE
-
-G_END_DECLS
 
 #endif // CLOUDPROVIDERS_H
