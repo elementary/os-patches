GNOME/libhandy

carousel: Add allow-long-swipes property
Expose the HdySwipeTracker property, as it's relevant here.

Signed-off-by: Alexander Mikhaylenko <alexm@gnome.org>

tests/test-carousel.c
@@ -285,6 +285,32 @@ test_hdy_carousel_allow_mouse_drag (void)
 }
 
 static void
+test_hdy_carousel_allow_long_swipes (void)
+{
+  HdyCarousel *carousel = HDY_CAROUSEL (hdy_carousel_new ());
+  gboolean allow_long_swipes;
+
+  notified = 0;
+  g_signal_connect (carousel, "notify::allow-long-swipes", G_CALLBACK (notify_cb), NULL);
+
+  /* Accessors */
+  g_assert_true (hdy_carousel_get_allow_long_swipes (carousel));
+  hdy_carousel_set_allow_long_swipes (carousel, FALSE);
+  g_assert_false (hdy_carousel_get_allow_long_swipes (carousel));
+  g_assert_cmpint (notified, ==, 1);
+
+  /* Property */
+  g_object_set (carousel, "allow-long-swipes", TRUE, NULL);
+  g_object_get (carousel, "allow-long-swipes", &allow_long_swipes, NULL);
+  g_assert_true (allow_long_swipes);
+  g_assert_cmpint (notified, ==, 2);
+
+  /* Setting the same value should not notify */
+  hdy_carousel_set_allow_long_swipes (carousel, TRUE);
+  g_assert_cmpint (notified, ==, 2);
+}
+
+static void
 test_hdy_carousel_reveal_duration (void)
 {
   HdyCarousel *carousel = HDY_CAROUSEL (hdy_carousel_new ());
@@ -326,6 +352,7 @@ main (gint argc,
   g_test_add_func("/Handy/Carousel/spacing", test_hdy_carousel_spacing);
   g_test_add_func("/Handy/Carousel/animation_duration", test_hdy_carousel_animation_duration);
   g_test_add_func("/Handy/Carousel/allow_mouse_drag", test_hdy_carousel_allow_mouse_drag);
+  g_test_add_func("/Handy/Carousel/allow_long_swipes", test_hdy_carousel_allow_long_swipes);
   g_test_add_func("/Handy/Carousel/reveal_duration", test_hdy_carousel_reveal_duration);
   return g_test_run();
 }

elementary/os-patches

tests/test-carousel.c
@@ -203,6 +203,32 @@ test_hdy_carousel_allow_mouse_drag (void)
 }
 
 static void
+test_hdy_carousel_allow_long_swipes (void)
+{
+  HdyCarousel *carousel = HDY_CAROUSEL (hdy_carousel_new ());
+  gboolean allow_long_swipes;
+
+  notified = 0;
+  g_signal_connect (carousel, "notify::allow-long-swipes", G_CALLBACK (notify_cb), NULL);
+
+  /* Accessors */
+  g_assert_false (hdy_carousel_get_allow_long_swipes (carousel));
+  hdy_carousel_set_allow_long_swipes (carousel, TRUE);
+  g_assert_true (hdy_carousel_get_allow_long_swipes (carousel));
+  g_assert_cmpint (notified, ==, 1);
+
+  /* Property */
+  g_object_set (carousel, "allow-long-swipes", FALSE, NULL);
+  g_object_get (carousel, "allow-long-swipes", &allow_long_swipes, NULL);
+  g_assert_false (allow_long_swipes);
+  g_assert_cmpint (notified, ==, 2);
+
+  /* Setting the same value should not notify */
+  hdy_carousel_set_allow_long_swipes (carousel, FALSE);
+  g_assert_cmpint (notified, ==, 2);
+}
+
+static void
 test_hdy_carousel_reveal_duration (void)
 {
   HdyCarousel *carousel = HDY_CAROUSEL (hdy_carousel_new ());
@@ -241,6 +267,7 @@ main (gint argc,
   g_test_add_func("/Handy/Carousel/spacing", test_hdy_carousel_spacing);
   g_test_add_func("/Handy/Carousel/animation_duration", test_hdy_carousel_animation_duration);
   g_test_add_func("/Handy/Carousel/allow_mouse_drag", test_hdy_carousel_allow_mouse_drag);
+  g_test_add_func("/Handy/Carousel/allow_long_swipes", test_hdy_carousel_allow_long_swipes);
   g_test_add_func("/Handy/Carousel/reveal_duration", test_hdy_carousel_reveal_duration);
   return g_test_run();
 }