3cd9a860963ed2e72af6ac09aec7f5dd4291b168
stsdc <6031763+stsdc@users.noreply.github.com>
2024-11-11 17:53:38 +0100

    refactor

--- get-latest-version.py
+++ get-latest-version.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python3
 
 import os
-import sys
-import apt_pkg
-from launchpadlib.launchpad import Launchpad
-from github import Github
 import subprocess
-from debian import deb822
-
-import urllib.request
+import sys
 import tarfile
-import os
+import urllib.request
 
-default_series_name = "bionic"
+import apt_pkg
+from debian import deb822
+from github import Github
+from launchpadlib.launchpad import Launchpad
+
+DEFAULT_SERIES_NAME = "bionic"
 
 # Process the command line arguments
 if len(sys.argv) < 2:
     raise ValueError("Please provide a package name")
 
 if len(sys.argv) < 3:
-    series_name = default_series_name
+    series_name = DEFAULT_SERIES_NAME
 else:
     series_name = sys.argv[2]
 
 if not series_name:
-    series_name = default_series_name
+    series_name = DEFAULT_SERIES_NAME
 
 if len(sys.argv) < 4:
     upstream_series_name = series_name
@@ -41,7 +40,10 @@ apt_pkg.init_system()
 
 # Initialize Launchpad variables
 launchpad = Launchpad.login_anonymously(
-    "elementary daily test", "production", "~/.launchpadlib/cache/", version="devel"
+    "elementary daily test",
+    "production",
+    "~/.launchpadlib/cache/",
+    version="devel"
 )
 
 ubuntu = launchpad.distributions["ubuntu"]
@@ -58,18 +60,17 @@ github_repo = os.environ["GITHUB_REPOSITORY"]
 github = Github(github_token)
 repo = github.get_repo(github_repo)
 
-subprocess.run(["git", "config", "--global", "user.email", "6031763+stsdc@users.noreply.github.com"])
-subprocess.run(["git", "config", "--global", "user.name", "stsdc"])
-subprocess.run(["git", "config", "--global", "--add", "safe.directory", "/__w/os-patches/os-patches"])
+subprocess.run(["git", "config", "--global", "user.email", "github-actions[bot]@users.noreply.github.com"], check=False)
+subprocess.run(["git", "config", "--global", "user.name", "github-actions[bot]"], check=False)
+subprocess.run(["git", "config", "--global", "--add", "safe.directory", "/__w/os-patches/os-patches"], check=False)
 
 # Method for checking if GitHub Actions has already opened an issue with this title
 def github_issue_exists(title):
     open_issues = repo.get_issues(state="open")
-    for issue in open_issues:
-        if issue.title == title and issue.user.login == "github-actions[bot]":
+    for open_issue in open_issues:
+        if open_issue.title == title and open_issue.user.login == "github-actions[bot]":
             return True
     return False
-
 
 
 def download_file(url, local_filename):
@@ -82,10 +83,9 @@ def extract_archive(file_path, extract_to='.'):
     # Open the archive file
     with tarfile.open(file_path, 'r:xz') as tar:
         members = tar.getmembers()
-        
         # Check if there's only one top-level folder
         top_level_dirs = [member for member in members if member.isdir() and '/' not in member.name.strip('/')]
-        
+
         if len(top_level_dirs) == 1:
             # If only one top-level directory, extract its contents
             top_level_dir_name = top_level_dirs[0].name
@@ -100,16 +100,27 @@ def extract_archive(file_path, extract_to='.'):
     os.remove(file_path)
 
 
+def get_patched_sources():
+    """Get the current version of a package in elementary os patches PPA"""
+    return patches_archive.getPublishedSources(
+        exact_match=True,
+        source_name=component_name,
+        status="Published",
+        distro_series=series,
+    )
+
+def get_upstream_sources():
+    """Get the current version of a package in upstream PPA"""
+    return ubuntu_archive.getPublishedSources(
+        exact_match=True,
+        source_name=component_name,
+        status="Published",
+        pocket=pocket,
+        distro_series=upstream_series,
+    )
 
 
-# Get the current version of a package in elementary os patches PPA
-patched_sources = patches_archive.getPublishedSources(
-    exact_match=True,
-    source_name=component_name,
-    status="Published",
-    distro_series=series,
-)
-if len(patched_sources) == 0:
+if len(get_patched_sources()) == 0:
     issue_title = "Package `%s` not found in os-patches PPA" % (component_name)
     if not github_issue_exists(issue_title):
         issue = repo.create_issue(
@@ -123,20 +134,13 @@ if len(patched_sources) == 0:
         )
     sys.exit(0)
 
-patched_version = patched_sources[0].source_package_version
+patched_version = get_patched_sources()[0].source_package_version
 
 # Search for a new version in the Ubuntu repositories
-pockets = ["Release", "Security", "Updates"]
-for pocket in pockets:
-    found_sources = ubuntu_archive.getPublishedSources(
-        exact_match=True,
-        source_name=component_name,
-        status="Published",
-        pocket=pocket,
-        distro_series=upstream_series,
-    )
-    if len(found_sources) > 0:
-        pocket_version = found_sources[0].source_package_version
+for pocket in ["Release", "Security", "Updates"]:
+    upstream_sources = get_upstream_sources()
+    if len(upstream_sources) > 0:
+        pocket_version = upstream_sources[0].source_package_version
         if apt_pkg.version_compare(pocket_version, patched_version) > 0:
             issue_title = "New version of %s available [%s]" % (
                 component_name,
@@ -155,7 +159,7 @@ for pocket in pockets:
 
                 url = "%s/+files/%s_%s.dsc" % (
                         ubuntu_archive.web_link,
-                        found_sources[0].source_package_name,
+                        upstream_sources[0].source_package_name,
                         pocket_version,
                     )
                 
