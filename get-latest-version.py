3ee5481819ae7242bd3614b988c083882a48a6c9
stsdc <6031763+stsdc@users.noreply.github.com>
2024-11-13 20:48:51 +0100

    use raw string

--- get-latest-version.py
+++ get-latest-version.py
@@ -132,7 +132,7 @@ for pocket in ["Release", "Security", "Updates"]:
 
                 subprocess.run(["git", "checkout", "-b", new_branch], check=True)
 
-                subprocess.run(["sed", "-i", "'s/^#\s*\(deb-src.*\)/\1/'", "/etc/apt/sources.list"], check=True)
+                subprocess.run(["sed", "-i", r"'s/^#\s*\(deb-src.*\)/\1/'", "/etc/apt/sources.list"], check=True)
 
                 subprocess.run(["apt", "source", component_name], check=True)
                 subprocess.run(["rm", "*.tar.*", "*.dsc"], check=True)
