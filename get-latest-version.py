3a7d98fb35b3cee9a093821d7a539d267786d548
stsdc <6031763+stsdc@users.noreply.github.com>
2024-11-13 22:35:17 +0100

    .

--- get-latest-version.py
+++ get-latest-version.py
@@ -133,7 +133,7 @@ for pocket in ["Release", "Security", "Updates"]:
                 subprocess.run(["git", "checkout", "-b", new_branch], check=True)
 
                 subprocess.run(["apt", "source", component_name], check=True)
-                subprocess.run(["rm", "*.tar.*", "*.dsc"], check=True)
+                # subprocess.run(["rm", "*.tar.*", "*.dsc"], check=True)
 
                 subprocess.run(["cp", "-r", f"{component_name}/*", "."], check=True)
                 subprocess.run(["rm", "-r", f"{component_name}"], check=True)
